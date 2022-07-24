09b780e27eb82285b4dfacf5684185bac854c46a
Conor Haining <conor.haining@gmail.com>
2022-07-24 18:13:26 +0200

    todo: offline hook tests

---
+++ hooks/useOffline.test.tsx
@@ -0,0 +1,7 @@
+import useOffline from "./useOffline";
+
+describe.skip("useOffline", () => {
+    it.todo("should set the state to false when an online event is fired");
+
+    it.todo("should set the state to true when an offline event is fired");
+});
