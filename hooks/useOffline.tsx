fdd4366061b31be6d3c32a5729d6cd4ea24d6af6
Conor Haining <conor.haining@gmail.com>
2022-07-24 18:20:03 +0200

    fix: cleanup for offline hook

--- hooks/useOffline.tsx
+++ hooks/useOffline.tsx
@@ -3,13 +3,22 @@ import { useEffect, useState } from "react";
 const useOffline = () => {
     const [isOffline, setIsOffline] = useState(false);
 
+    const onOnline = () => {
+        setIsOffline(false);
+    }
+
+    const onOffline = () => {
+        setIsOffline(true);
+    }
+
     useEffect(() => {
-        window.addEventListener("online", () => {
-            setIsOffline(false);
-        });
-        window.addEventListener("offline", () => {
-            setIsOffline(true);
-        });
+        window.addEventListener("online", onOnline);
+        window.addEventListener("offline", onOffline);
+
+        return () => {
+            window.removeEventListener("online", onOnline);
+            window.removeEventListener("offline", onOffline);
+        }
     }, []);
 
     return [isOffline];
