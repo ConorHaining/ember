f51813caa4fd3a4de6a17a8663c0880b2401162b
Conor Haining <conor.haining@gmail.com>
2022-07-25 10:46:35 +0200

    feat: live updating for stop info, based on network speed

--- pages/trips/[tripId].tsx
+++ pages/trips/[tripId].tsx
@@ -1,39 +1,47 @@
 import { GetServerSidePropsContext, NextPage } from "next";
+import useSWR, { SWRConfig } from 'swr';
 import { StopPoint } from "../../components/StopPoint";
 import WebShareButton from "../../components/WebShareButton";
 import useNetworkInformation from "../../hooks/useNetworkInformation";
 import useOffline from "../../hooks/useOffline";
 
-type TripPageProps = {
-    description: {
-        calendar_date: string,
-        route_number: string
-    },
-    route: {
+type StopPointData = {
+    id: number;
+    departure: {
+        scheduled: string;
+        estimated: string;
+        actual: string;
+    };
+    arrival: {
+        scheduled: string;
+        estimated: string;
+        actual: string;
+    };
+    location: {
         id: number;
-        departure: {
-            scheduled: string;
-            estimated: string;
-            actual: string
-        };
-        arrival: {
-            scheduled: string;
-            estimated: string;
-            actual: string
-        };
-        location: {
-            id: number;
-            name: string;
-        };
-        skipped: boolean;
-        booking_cut_off_mins: number
-    }[]
+        name: string;
+    };
+    skipped: boolean;
+    booking_cut_off_mins: number;
 };
 
-const TripPage: NextPage<TripPageProps> = ({ route, description }) => {
+type TripPageProps = {
+    description: {
+        calendar_date: string;
+        route_number: string;
+        tripId: string;
+    },
+    fallback: {
+        route: StopPointData[]
+    }
+};
+
+const fetcher = (...args) => fetch(...args).then(res => res.json());
+
+const TripPage: NextPage<TripPageProps> = ({ fallback, description }) => {
 
     const [isOffline] = useOffline();
-    const [networkType] = useNetworkInformation();
+    const [networkType, effectiveNetworkType] = useNetworkInformation();
 
     return (
         <>
@@ -50,27 +58,50 @@ const TripPage: NextPage<TripPageProps> = ({ route, description }) => {
                     </div>
                     <div>
                         <WebShareButton tripId={description.tripId} />
+
                     </div>
                 </header>
                 <section>
-                    {route.map((point, index, array) => (
-                        <div key={point.location.id} className="my-4">
-                            <StopPoint
-                                destination={point.location.name}
-                                estimatedDepartureTime={point.departure.estimated}
-                                actualDepartureTime={point.departure.actual}
-                                scheduledDepartureTime={point.departure.scheduled}
-                                actualArrivalTime={point.arrival.actual}
-                                scheduledArrivalTime={point.arrival.scheduled}
-                                isSkipped={point.skipped}
-                                reservationCutoffInMinutes={point.booking_cut_off_mins} 
-                                isTerminating={array.length - 1 === index} />
-                        </div>
-                    ))}
+                        <SWRConfig value={{ fallback }}>
+                            <StopList tripId={description.tripId} />
+                        </SWRConfig>
                 </section>
             </main>
         </>
     )
+}
+
+const StopList: React.FC<{tripId: string}> = ({tripId}) => {
+
+    const [networkType, effectiveNetworkType] = useNetworkInformation();
+
+    let refreshInterval;
+    if (networkType === 'wifi') {
+        refreshInterval = 5000;
+    } else if (effectiveNetworkType === '4g') {
+        refreshInterval = 15000;
+    } else {
+        refreshInterval = 30000;
+    }
+
+    const { data, error } = useSWR(`https://api.ember.to/v1/trips/${tripId}`, fetcher, { refreshInterval });
+
+    return (
+        data.route.map((point, index, array) => (
+            <div key={point.location.id} className="my-4">
+                <StopPoint
+                    destination={point.location.name}
+                    estimatedDepartureTime={point.departure.estimated}
+                    actualDepartureTime={point.departure.actual}
+                    scheduledDepartureTime={point.departure.scheduled}
+                    actualArrivalTime={point.arrival.actual}
+                    scheduledArrivalTime={point.arrival.scheduled}
+                    isSkipped={point.skipped}
+                    reservationCutoffInMinutes={point.booking_cut_off_mins}
+                    isTerminating={array.length - 1 === index} />
+            </div>
+        ))
+    );
 }
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
@@ -100,9 +131,14 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
         calendar_date: data.description.calendar_date,
         tripId
     }
+
+    const apiUrl = `https://api.ember.to/v1/trips/${tripId}`;
+
     return {
         props: {
-            route,
+            fallback: {
+                [apiUrl]: route
+            },
             description
         }, // will be passed to the page component as props
     }
