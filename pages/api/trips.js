1f9bb9e39debe86ebdf040442e2e3df84f2b6bfd
Conor Haining <conor.haining@gmail.com>
2022-07-25 20:26:28 +0200

    fix: make dates ISO standard

--- pages/api/trips.js
+++ pages/api/trips.js
@@ -1,359 +1,359 @@
 const data = [
   {
     "uid": "MjpPJTSdSaeJdenn6YA5i2",
-    "departure": "2022-07-25 14:00:00"
+    "departure": "2022-07-25T14:00:00.000Z"
   },
   {
     "uid": "ZxzYfMMJF9Z4NYN2MsrYo3",
-    "departure": "2022-07-25 15:30:00"
+    "departure": "2022-07-25T15:30:00.000Z"
   },
   {
     "uid": "jXBZ7ZRk3toMSnG6wQH26z",
-    "departure": "2022-07-25 17:00:00"
+    "departure": "2022-07-25T17:00:00.000Z"
   },
   {
     "uid": "7wDjW55LX5vhxmMk7ws2So",
-    "departure": "2022-07-25 18:30:00"
+    "departure": "2022-07-25T18:30:00.000Z"
   },
   {
     "uid": "gmVwcJ8u8xcEoXXVytCiFe",
-    "departure": "2022-07-25 20:00:00"
+    "departure": "2022-07-25T20:00:00.000Z"
   },
   {
     "uid": "4yBrLAiYy4GbLdFDunfDB9",
-    "departure": "2022-07-25 21:30:00"
+    "departure": "2022-07-25T21:30:00.000Z"
   },
   {
     "uid": "6mQX3J2eV53D33LoA9ftVS",
-    "departure": "2022-07-25 23:00:00"
+    "departure": "2022-07-25T23:00:00.000Z"
   },
   {
     "uid": "JEFSpsjYQGavC3BmgMAo7s",
-    "departure": "2022-07-25 14:34:00"
+    "departure": "2022-07-25T14:34:00.000Z"
   },
   {
     "uid": "o4hMpiyiRgjC2m69B6M7os",
-    "departure": "2022-07-25 16:03:00"
+    "departure": "2022-07-25T16:03:00.000Z"
   },
   {
     "uid": "aA9YcbNVDdwVSi7PwQsLXv",
-    "departure": "2022-07-25 17:34:00"
+    "departure": "2022-07-25T17:34:00.000Z"
   },
   {
     "uid": "ZrDA9zZ92LY5hm5FUY2JJa",
-    "departure": "2022-07-25 19:01:00"
+    "departure": "2022-07-25T19:01:00.000Z"
   },
   {
     "uid": "UszXMuTMzUyzSjKCpjG7fZ",
-    "departure": "2022-07-25 20:24:00"
+    "departure": "2022-07-25T20:24:00.000Z"
   },
   {
     "uid": "kBPeywsVbAfZxw6SvpdZbR",
-    "departure": "2022-07-25 21:47:00"
+    "departure": "2022-07-25T21:47:00.000Z"
   },
   {
     "uid": "VKQGgGzedSret7axEKX6GS",
-    "departure": "2022-07-25 23:16:00"
+    "departure": "2022-07-25T23:16:00.000Z"
   },
   {
     "uid": "8ev2gDeAmamKutCrxcfZs6",
-    "departure": "2022-07-26 00:44:00"
+    "departure": "2022-07-26T00:44:00.000Z"
   },
   {
     "uid": "5EMYgoLYBxpypCC3ZMY6Du",
-    "departure": "2022-07-26 02:00:00"
+    "departure": "2022-07-26T02:00:00.000Z"
   },
   {
     "uid": "cm67NadBLTiDv7bepU4Bc6",
-    "departure": "2022-07-26 05:00:00"
+    "departure": "2022-07-26T05:00:00.000Z"
   },
   {
     "uid": "fGVgmMvx2jAcrvKUxZ8T5X",
-    "departure": "2022-07-26 07:00:00"
+    "departure": "2022-07-26T07:00:00.000Z"
   },
   {
     "uid": "nF9jdx3LCcwqKJVF5HADrN",
-    "departure": "2022-07-26 08:15:00"
+    "departure": "2022-07-26T08:15:00.000Z"
   },
   {
     "uid": "jqfhCsWzru9v4Z4uC8gPNZ",
-    "departure": "2022-07-26 09:30:00"
+    "departure": "2022-07-26T09:30:00.000Z"
   },
   {
     "uid": "WMLwBmXL5mnv4WqHYiL5ZE",
-    "departure": "2022-07-26 11:00:00"
+    "departure": "2022-07-26T11:00:00.000Z"
   },
   {
     "uid": "FuZdDshPMgcqkF3ADFmbTD",
-    "departure": "2022-07-26 12:30:00"
+    "departure": "2022-07-26T12:30:00.000Z"
   },
   {
     "uid": "H29LwAVp5mKjec53jGHoDf",
-    "departure": "2022-07-26 14:00:00"
+    "departure": "2022-07-26T14:00:00.000Z"
   },
   {
     "uid": "9rcVoz7jzSjUKiwqfSFvfv",
-    "departure": "2022-07-26 15:30:00"
+    "departure": "2022-07-26T15:30:00.000Z"
   },
   {
     "uid": "N6eoKezPAEaD2z8ix5Z43a",
-    "departure": "2022-07-26 17:00:00"
+    "departure": "2022-07-26T17:00:00.000Z"
   },
   {
     "uid": "eR3W6VKrqqH3d89BczwThX",
-    "departure": "2022-07-26 18:30:00"
+    "departure": "2022-07-26T18:30:00.000Z"
   },
   {
     "uid": "7VnP2U3BgQrzM6eBwVNdiK",
-    "departure": "2022-07-26 20:00:00"
+    "departure": "2022-07-26T20:00:00.000Z"
   },
   {
     "uid": "9DnVX4ZR5rnjmDTJvuaGFq",
-    "departure": "2022-07-26 21:30:00"
+    "departure": "2022-07-26T21:30:00.000Z"
   },
   {
     "uid": "iS5yDyJXnRaB8izz9yqMbf",
-    "departure": "2022-07-26 23:00:00"
+    "departure": "2022-07-26T23:00:00.000Z"
   },
   {
     "uid": "jjK2dHgCYBgfJ5ZQ93TB4m",
-    "departure": "2022-07-26 03:43:00"
+    "departure": "2022-07-26T03:43:00.000Z"
   },
   {
     "uid": "EtYFJpm7mxce4jrCWULhLt",
-    "departure": "2022-07-26 06:47:00"
+    "departure": "2022-07-26T06:47:00.000Z"
   },
   {
     "uid": "Yr9UBwMfep3vyvwDGHxsrD",
-    "departure": "2022-07-26 08:58:00"
+    "departure": "2022-07-26T08:58:00.000Z"
   },
   {
     "uid": "M4sNZLaFFDfu5YDARCJnMc",
-    "departure": "2022-07-26 10:15:00"
+    "departure": "2022-07-26T10:15:00.000Z"
   },
   {
     "uid": "FCd5PiFKDknMDV7Li9aN5j",
-    "departure": "2022-07-26 11:35:00"
+    "departure": "2022-07-26T11:35:00.000Z"
   },
   {
     "uid": "4apMXYV9pWLbyU5CV3X2JK",
-    "departure": "2022-07-26 13:05:00"
+    "departure": "2022-07-26T13:05:00.000Z"
   },
   {
     "uid": "A3MDeWjsRMjAR4r9BQLjbX",
-    "departure": "2022-07-26 14:34:00"
+    "departure": "2022-07-26T14:34:00.000Z"
   },
   {
     "uid": "8b7vCpHnwgBucgRbovx7YJ",
-    "departure": "2022-07-26 16:03:00"
+    "departure": "2022-07-26T16:03:00.000Z"
   },
   {
     "uid": "ZKX8XjMQKFnY58Bo82Eiyf",
-    "departure": "2022-07-26 17:34:00"
+    "departure": "2022-07-26T17:34:00.000Z"
   },
   {
     "uid": "CJXADkx6GN9KND9DpzL95n",
-    "departure": "2022-07-26 19:01:00"
+    "departure": "2022-07-26T19:01:00.000Z"
   },
   {
     "uid": "BEqnNo4pU3LG3uT4ayx6XQ",
-    "departure": "2022-07-26 20:24:00"
+    "departure": "2022-07-26T20:24:00.000Z"
   },
   {
     "uid": "PivuAn6mGkXDj88B2J7PVV",
-    "departure": "2022-07-26 21:47:00"
+    "departure": "2022-07-26T21:47:00.000Z"
   },
   {
     "uid": "MmAByeAgrLSZwyUuJ6dxiD",
-    "departure": "2022-07-26 23:16:00"
+    "departure": "2022-07-26T23:16:00.000Z"
   },
   {
     "uid": "dmme3NgXLiQ2GKMrDeZMDd",
-    "departure": "2022-07-27 00:44:00"
+    "departure": "2022-07-27T00:44:00.000Z"
   },
   {
     "uid": "PFKcHvd2UxpXnaDirCA9sy",
-    "departure": "2022-07-27 02:00:00"
+    "departure": "2022-07-27T02:00:00.000Z"
   },
   {
     "uid": "JxewRUxFTeMX362dnYuVwA",
-    "departure": "2022-07-27 05:00:00"
+    "departure": "2022-07-27T05:00:00.000Z"
   },
   {
     "uid": "fBfRjay2oJRvhHiCSa69cv",
-    "departure": "2022-07-27 07:00:00"
+    "departure": "2022-07-27T07:00:00.000Z"
   },
   {
     "uid": "PJU8a5u2w4F9PjDCd6Nhnj",
-    "departure": "2022-07-27 08:15:00"
+    "departure": "2022-07-27T08:15:00.000Z"
   },
   {
     "uid": "Gwv3tHpLzNCfdh2EzVkmxr",
-    "departure": "2022-07-27 09:30:00"
+    "departure": "2022-07-27T09:30:00.000Z"
   },
   {
     "uid": "aQWH9xtcRep85wanqAWJCi",
-    "departure": "2022-07-27 11:00:00"
+    "departure": "2022-07-27T11:00:00.000Z"
   },
   {
     "uid": "NsyjrWghjZawrRxRcsxfb7",
-    "departure": "2022-07-27 12:30:00"
+    "departure": "2022-07-27T12:30:00.000Z"
   },
   {
     "uid": "2KUSZbduBEYNARHYLfjxuB",
-    "departure": "2022-07-27 14:00:00"
+    "departure": "2022-07-27T14:00:00.000Z"
   },
   {
     "uid": "EVTUXmSzW35uDLyCzf7yZy",
-    "departure": "2022-07-27 15:30:00"
+    "departure": "2022-07-27T15:30:00.000Z"
   },
   {
     "uid": "MC9rqwwfHgkqrosXjkHhHL",
-    "departure": "2022-07-27 17:00:00"
+    "departure": "2022-07-27T17:00:00.000Z"
   },
   {
     "uid": "jYhxpKqvpnp8wWd8tpMvGg",
-    "departure": "2022-07-27 18:30:00"
+    "departure": "2022-07-27T18:30:00.000Z"
   },
   {
     "uid": "NPWUeTkJXrbFdgfvu9wXE9",
-    "departure": "2022-07-27 20:00:00"
+    "departure": "2022-07-27T20:00:00.000Z"
   },
   {
     "uid": "94AKnoadwtkMdtcCiJ58Db",
-    "departure": "2022-07-27 21:30:00"
+    "departure": "2022-07-27T21:30:00.000Z"
   },
   {
     "uid": "fL2tRXDsZqTkdW2t7YhmpD",
-    "departure": "2022-07-27 23:00:00"
+    "departure": "2022-07-27T23:00:00.000Z"
   },
   {
     "uid": "aBvDkvXWqm9KNmXnsoVJxX",
-    "departure": "2022-07-27 03:43:00"
+    "departure": "2022-07-27T03:43:00.000Z"
   },
   {
     "uid": "hYuqR2kZpPZtheBaLyd3ST",
-    "departure": "2022-07-27 06:47:00"
+    "departure": "2022-07-27T06:47:00.000Z"
   },
   {
     "uid": "Jjdw4npuPXU7pgCKtFredz",
-    "departure": "2022-07-27 08:58:00"
+    "departure": "2022-07-27T08:58:00.000Z"
   },
   {
     "uid": "kD6hEUx5e5XzgPHwWPYmZr",
-    "departure": "2022-07-27 10:15:00"
+    "departure": "2022-07-27T10:15:00.000Z"
   },
   {
     "uid": "PEdrBKVRTPotNWxybNsWF7",
-    "departure": "2022-07-27 11:35:00"
+    "departure": "2022-07-27T11:35:00.000Z"
   },
   {
     "uid": "UHz2GyCGG9WjfaeiVuxrhD",
-    "departure": "2022-07-27 13:05:00"
+    "departure": "2022-07-27T13:05:00.000Z"
   },
   {
     "uid": "DSfLyvah3n6kDfoxbjsrPv",
-    "departure": "2022-07-27 14:34:00"
+    "departure": "2022-07-27T14:34:00.000Z"
   },
   {
     "uid": "mTD2QGebxejwWKWFKZ8MfF",
-    "departure": "2022-07-27 16:03:00"
+    "departure": "2022-07-27T16:03:00.000Z"
   },
   {
     "uid": "CC8LTmWAfnoPWzFZe8j9NK",
-    "departure": "2022-07-27 17:34:00"
+    "departure": "2022-07-27T17:34:00.000Z"
   },
   {
     "uid": "mUmXoxfWTvVdJP5wawSyQw",
-    "departure": "2022-07-27 19:01:00"
+    "departure": "2022-07-27T19:01:00.000Z"
   },
   {
     "uid": "SahtvjwDZCgr5LkDzxrtPB",
-    "departure": "2022-07-27 20:24:00"
+    "departure": "2022-07-27T20:24:00.000Z"
   },
   {
     "uid": "6xcDnh7pxm5EcHB39knToi",
-    "departure": "2022-07-27 21:47:00"
+    "departure": "2022-07-27T21:47:00.000Z"
   },
   {
     "uid": "njN8Qq2eVTYL3HYdkNL5rb",
-    "departure": "2022-07-27 23:16:00"
+    "departure": "2022-07-27T23:16:00.000Z"
   },
   {
     "uid": "Gm8fh7zm9sHvivimk8TyaV",
-    "departure": "2022-07-25 15:50:00"
+    "departure": "2022-07-25T15:50:00.000Z"
   },
   {
     "uid": "WCwVm26cSyB7YrmQA7DYP9",
-    "departure": "2022-07-25 16:05:00"
+    "departure": "2022-07-25T16:05:00.000Z"
   },
   {
     "uid": "K6cGwQCJ4j3cH5fHvhApQX",
-    "departure": "2022-07-25 18:18:00"
+    "departure": "2022-07-25T18:18:00.000Z"
   },
   {
     "uid": "C4DyHURbbB3jxEjVAT3yNm",
-    "departure": "2022-07-26 06:40:00"
+    "departure": "2022-07-26T06:40:00.000Z"
   },
   {
     "uid": "8VtBJgCeLAJTpz6Rtg55SA",
-    "departure": "2022-07-26 08:53:00"
+    "departure": "2022-07-26T08:53:00.000Z"
   },
   {
     "uid": "8pRNUaUFDCHpKsf9SpqYXR",
-    "departure": "2022-07-26 09:10:00"
+    "departure": "2022-07-26T09:10:00.000Z"
   },
   {
     "uid": "CCUVwXfGY2cPoLgnycf8G8",
-    "departure": "2022-07-26 11:23:00"
+    "departure": "2022-07-26T11:23:00.000Z"
   },
   {
     "uid": "EQxBFD74nT9d6g7caAqnUq",
-    "departure": "2022-07-26 13:40:00"
+    "departure": "2022-07-26T13:40:00.000Z"
   },
   {
     "uid": "EDrfcSKiwqfQ69AS5aq9oQ",
-    "departure": "2022-07-26 15:50:00"
+    "departure": "2022-07-26T15:50:00.000Z"
   },
   {
     "uid": "ZxMTP3kcxsdqn3Xhfi3PdY",
-    "departure": "2022-07-26 16:05:00"
+    "departure": "2022-07-26T16:05:00.000Z"
   },
   {
     "uid": "W3rfGUfCoZRaW4R3nPtGpu",
-    "departure": "2022-07-26 18:18:00"
+    "departure": "2022-07-26T18:18:00.000Z"
   },
   {
     "uid": "grqEFVWoVXAGz5TBYNGaq5",
-    "departure": "2022-07-27 06:40:00"
+    "departure": "2022-07-27T06:40:00.000Z"
   },
   {
     "uid": "kaapJzMmQNhGbsfEHBn5cR",
-    "departure": "2022-07-27 08:53:00"
+    "departure": "2022-07-27T08:53:00.000Z"
   },
   {
     "uid": "4d9a9rBaVjEVvBwUXWBB9Z",
-    "departure": "2022-07-27 09:10:00"
+    "departure": "2022-07-27T09:10:00.000Z"
   },
   {
     "uid": "DmbUkXdwYWFDXVjn4KmKJk",
-    "departure": "2022-07-27 11:23:00"
+    "departure": "2022-07-27T11:23:00.000Z"
   },
   {
     "uid": "Lg2kSr6nmm7YtXfUMwQPJh",
-    "departure": "2022-07-27 13:40:00"
+    "departure": "2022-07-27T13:40:00.000Z"
   },
   {
     "uid": "esTXwziQUjQ5XBfHnYQu3b",
-    "departure": "2022-07-27 15:50:00"
+    "departure": "2022-07-27T15:50:00.000Z"
   },
   {
     "uid": "jb7tYekYsZi7Yg8zyN3v8i",
-    "departure": "2022-07-27 16:05:00"
+    "departure": "2022-07-27T16:05:00.000Z"
   },
   {
     "uid": "7yqzosDgmgJYHEWjwqT4uo",
-    "departure": "2022-07-27 18:18:00"
+    "departure": "2022-07-27T18:18:00.000Z"
   }
  ];
 
